c8d8a82841c4bd5832e8aecc60dbf6a488ff6547
matheusnarciso@hotmail.com <matheusnarciso@hotmail.com>
2020-01-28 14:24:48 -0300

    iniciando registro 0001 e 0035

---
+++ source/c#/lab/lab.Domain/Entities/Bloco/b0/Reg0001EFDC.cs
@@ -0,0 +1,10 @@
+﻿using lab.Domain.Entities.Enums.Indicadores;
+
+namespace lab.Domain.Entities.Bloco.b0
+{
+    public class Reg0001EFDC
+    {
+        public string Reg { get; set; }
+        public IndicadorMovimento IndicadorMovimento { get; set; }
+    }
+}
---
+++ source/c#/lab/lab.Domain/Entities/Bloco/b0/Reg0035EFDC.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab.Domain.Entities.Bloco.b0
+{
+    public class Reg0035EFDC
+    {
+        public int Id { get; set; }
+        public string Reg { get; set; }
+        public int CodSCP { get; set; }
+        public string DescSCP { get; set; }
+        public string InfoCompl { get; set; }
+    }
+}
