c213d5c7dc699ce5df6dba3e3f602761034fb59a
Matheus <matheusnarciso@hotmail.com>
2020-02-03 18:00:37 -0300

    só os arquivos dos novos registros

---
+++ source/c#/lab/lab.Domain/Entities/Bloco/b0/Reg0450EFDC.cs
@@ -0,0 +1,21 @@
+﻿using lab.Domain.Entities.Cadastro.Auxiliar;
+
+namespace lab.Domain.Entities.Bloco.b0
+{
+    public class Reg0140EFDC
+    {
+        public int Id { get; set; }
+        public string Reg { get; set; }
+        public string CodEstab { get; set; }
+        public string Nome { get; set; }
+        public int CNPJ { get; set; }
+        public string UF { get; set; }
+        public string IE { get; set; }
+        public int MunicipioId { get; set; }
+        public virtual Municipio Municipio { get; set; }
+        public string IM { get; set; }
+        public string Suframa { get; set; }
+        public int Reg0000EFDCId { get; set; }
+        public virtual Reg0000EFDC Reg0000EFDC { get; set; }
+    }
+}
